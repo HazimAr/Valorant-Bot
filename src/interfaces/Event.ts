1e3fd9926033eb8ad0aa5b71f2877e0d6370a26a
Nik Schaefer <nikkschaefer@gmail.com>
2021-02-03 13:07:44 -0600

    Revert "boom"
    This reverts commit 621b862040db2e1ee0a683ae5ee0c93a0cb1cf44.

---
+++ src/interfaces/Event.ts
@@ -0,0 +1,12 @@
+import { Bot } from '../client/Client';
+
+
+export interface RunFunction {
+    (client: Bot, ...args: any[]): Promise<void>
+};
+
+export interface Event {
+    name: string;
+    run: RunFunction;
+};
+
