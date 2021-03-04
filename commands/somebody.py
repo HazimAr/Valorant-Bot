11d2842d0fef979ef2b43d414c64d88bc1c61635
Hazim Arafa <hazimarafa80@gmail.com>
2021-03-04 12:02:18 -0800

    new commands

--- commands/somebody.py
+++ commands/somebody.py
@@ -5,6 +5,8 @@ import discord
 
 import random
 
+from time import sleep
+
 class Somebody(BaseCommand):
 
     def __init__(self):
@@ -13,10 +15,15 @@ class Somebody(BaseCommand):
         super().__init__(description, params)
 
     async def handle(self, params, message, client):
-        member_list = message.guild.members
-        random_member = random.choice(list(member_list))
+        member_list = list(message.guild.members)
+        random_member = random.choice(member_list)
         random_member_id = f'<@{random_member.id}>'
-        print(list(member_list))
+        for i in range(len(member_list)):
+            member = member_list[i]
+            msg = f'<@{member.id}>'
+            print(msg)
+            sleep(0.5)
+            await message.channel.send(msg)
 
         msg = f"{random_member_id} has been chosen by the gods to be annoyed"
         await message.channel.send(msg)
--- settings.py
+++ settings.py
@@ -6,6 +6,7 @@ COMMAND_PREFIX = "b!"
 
 # The bot token. Keep this secret!
 BOT_TOKEN = "ODA2NjM3NjM4NjY0MDYwOTI5.YBsV1w.oNgELJmB_C9-feoMsS1lLxgdj8A"
+# BOT_TOKEN = "NzQ2NzgzOTQ4NTYyMTA0MzUx.X0FWvw.vmzwSaqYKl-G2opC-n4vU5QZV_E"
 
 # The now playing game. Set this to anything false-y ("", None) to disable it
 NOW_PLAYING = COMMAND_PREFIX + "help"
