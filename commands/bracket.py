c63b2d71a9c817af63d075e6ba0f1bec002f7dfb
Hazim Arafa <hazimarafa80@gmail.com>
2021-04-05 11:12:58 -0700

    Update bracket.py

--- commands/bracket.py
+++ commands/bracket.py
@@ -15,6 +15,7 @@ class Bracket(BaseCommand):
         users = ["Sakuraツ#juice", "SyfeFPS#8099", "SoulPsych0#uwu", "tunaflip#uwu", "Misaki#levi"]
         users_rank = []
         users_and_rank = {}
+
         for i in users:
             await message.channel.send(f"Checking {i}")
             user = i.split("#")[0]; tag = i.split("#")[1]
@@ -31,14 +32,34 @@ class Bracket(BaseCommand):
             if type(i) == int:
                 total_mmr += i
         average_user_mmr = total_mmr / len(users)
-        msg += f"The average mmr is: {average_user_mmr}"
+        msg += f"\nThe average mmr is: {average_user_mmr}\n"
 
         for key in users:
             for value in users_rank:
                 users_and_rank[key] = value
                 users_rank.remove(value)
                 break
+
+        users_and_rank = dict(filter(lambda elem: not(elem[1] == None), users_and_rank.items()))
         msg += str(users_and_rank)
 
-        
+        bracket = []
+        temp_key = ""
+
+        for i in range(len(users_and_rank.keys()) // 2):
+            users_and_rank_keys = list(users_and_rank.keys())
+            player_one_key = users_and_rank_keys[i]
+            users_and_rank_keys.remove(player_one_key)
+            users_and_rank_keys = list(users_and_rank.keys())
+
+            temp_key = users_and_rank_keys[i+1]
+
+            for i in users_and_rank_keys:
+                if (average_user_mmr - users_and_rank[i]) < (average_user_mmr - users_and_rank[temp_key]):
+                    temp_key = i
+
+            bracket.append([[player_one_key,users_and_rank[player_one_key]],[temp_key,users_and_rank[temp_key]]])
+               
+        print(bracket)
+            
         await message.channel.send(msg)
